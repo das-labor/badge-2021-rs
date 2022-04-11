4377dcad7637423b5f684ad014859c3abb982862
Daniel Maslowski <info@orangecms.org>
2022-04-11 18:50:11 +0200

    Blinky blinky

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@
 
 use core::fmt::Write;
 
-use esp32_hal::{pac::Peripherals, prelude::*, Serial, Timer};
+use esp32_hal::{gpio, pac::Peripherals, pac::GPIO, pac::IO_MUX, prelude::*, Serial, Timer};
 use nb::block;
 use panic_halt as _;
 use xtensa_lx_rt as _;
@@ -13,16 +13,27 @@ use xtensa_lx_rt::entry;
 fn main() -> ! {
     let peripherals = Peripherals::take().unwrap();
 
+    let mut rtccntl = peripherals.RTC_CNTL;
     let mut timer0 = Timer::new(peripherals.TIMG0);
+    let mut timer1 = Timer::new(peripherals.TIMG1);
     let mut serial0 = Serial::new(peripherals.UART0).unwrap();
 
     // Disable watchdog timer
     timer0.disable();
+    timer1.disable();
 
     timer0.start(10_000_000u64);
 
+    let io = gpio::IO::new(peripherals.GPIO, peripherals.IO_MUX);
+
+    let mut led = io.pins.gpio2.into_push_pull_output();
+
     loop {
-        writeln!(serial0, "Hello world!\r").unwrap();
+        writeln!(serial0, "====== ON =====\r").unwrap();
+        led.set_high().unwrap();
+        block!(timer0.wait()).unwrap();
+        writeln!(serial0, "===== OFF =====\r").unwrap();
+        led.set_low().unwrap();
         block!(timer0.wait()).unwrap();
     }
 }
