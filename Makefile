b3ff8415ebec84eaba4179216eb6ce5ae447a2b6
Daniel Maslowski <info@orangecms.org>
2023-11-03 00:43:27 +0100

    Upgrade crates, rework, add initial GPIO interrupt handling

--- Makefile
+++ Makefile
@@ -1,32 +1,13 @@
-# Note: You need to have the toolchain set up for all of this to work.
-#
-# TODO: Document how ^^
+ELF := target/xtensa-esp32-none-elf/debug/badge_2021_rs
 
-CMD := build
-PORT := /dev/ttyACM0
-
-_ESP_TOOLS := "$(HOME)/.espressif/tools"
-_CLANG_VER :="esp-13.0.0-20211203-x86_64-unknown-linux-gnu"
-_CLANG_DIR := "$(_ESP_TOOLS)/xtensa-esp32-elf-clang/$(_CLANG_VER)"
-_GCC_VER := "xtensa-esp32-elf-gcc8_4_0-esp-2021r2-patch3"
-_GCC_DIR := "$(_ESP_TOOLS)/xtensa-esp32-elf-gcc/$(_GCC_VER)"
-
-PATH := "$(_GCC_DIR)/bin:$(_CLANG_DIR)/bin:$(PATH)"
-
-LIBCLANG_PATH = "$(_CLANG_DIR)/lib"
-PIP_USER = no
-X_FEATURES = "xtensa-lx-rt/esp32"
-
-cargo:
-	PATH=$(PATH) LIBCLANG_PATH=$(LIBCLANG_PATH) \
-		cargo $(CMD) --features="$(X_FEATURES)" $(_EXTRA)
-
-release:
-	$(MAKE) cargo CMD="build --release"
-
-objdump:
-	$(_CLANG_DIR)/bin/xtensa-esp32-elf-objdump -D \
-	  target/xtensa-esp32-none-elf/debug/badge_2021_rs
+build:
+	. $(HOME)/export-esp.sh && \
+		cargo build --release
 
 flash:
-	$(MAKE) cargo CMD="espflash --speed=115200 --monitor" _EXTRA=$(PORT)
+	. $(HOME)/export-esp.sh && \
+		cargo espflash flash --release --monitor
+
+objdump:
+	. $(HOME)/export-esp.sh && \
+	xtensa-esp32-elf-objdump -D $(ELF)
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 #![no_std]
 #![no_main]
 
-// extern crate shared_bus;
-use core::{fmt::Write, str};
+use arrform::{arrform, ArrForm};
+use core::{borrow::BorrowMut, cell::RefCell};
+
 use embedded_graphics::mono_font::{
     ascii::{FONT_10X20, FONT_6X10},
     MonoTextStyle,
@@ -11,22 +12,58 @@ use embedded_graphics::pixelcolor::*;
 use embedded_graphics::prelude::*;
 use embedded_graphics::primitives::*;
 use embedded_graphics::text::*;
-use esp32::UART0;
-use esp32_hal::{gpio, i2c, pac::Peripherals, prelude::*, RtcCntl, Serial, Timer};
-use nb::block;
-use panic_write::PanicHandler;
+
+use esp32_hal::{
+    clock::ClockControl,
+    gpio::{Event, Gpio0, Gpio12, Gpio2, Gpio5, Input, Pin, PullUp, IO},
+    i2c::I2C,
+    interrupt,
+    peripherals::{Interrupt, Peripherals, I2C0, UART0},
+    prelude::*,
+    rtc_cntl::RtcClock,
+    timer::TimerGroup,
+    xtensa_lx, Cpu, Delay, Timer, Uart,
+};
+use esp_backtrace as _;
+use esp_println::println;
+
 use shared_bus;
-use ssd1306;
-use ssd1306::mode::DisplayConfig;
-use xtensa_lx_rt as _;
+use ssd1306::{self, mode::DisplayConfig, I2CDisplayInterface, Ssd1306};
+
+use xtensa_lx::mutex::{Mutex, SpinLockMutex};
 use xtensa_lx_rt::entry;
 
-fn draw<'a, D>(
-    display: &mut D,
-    s: &mut Serial<UART0>,
-    title: &'a str,
-    msg: &'a str,
-) -> Result<(), D::Error>
+// Used in example code, see
+// https://github.com/esp-rs/esp-hal/blob/main/esp32-hal/examples/gpio_interrupt.rs
+// use critical_section::Mutex;
+
+type LCD128x64<'a> = Ssd1306<
+    ssd1306::prelude::I2CInterface<shared_bus::I2cProxy<'a, I2C0>>,
+    ssd1306::size::DisplaySize128x64,
+    ssd1306::mode::BufferedGraphicsMode<ssd1306::size::DisplaySize128x64>,
+>;
+
+/*
+static PBTN1: Mutex<Option<Gpio0<Input<PullUp>>>> = Mutex::new(RefCell::new(None));
+static PBTN2: Mutex<Option<Gpio2<Input<PullUp>>>> = Mutex::new(RefCell::new(None));
+static JBTN1: Mutex<Option<Gpio5<Input<PullUp>>>> = Mutex::new(RefCell::new(None));
+static JBTN2: Mutex<Option<Gpio12<Input<PullUp>>>> = Mutex::new(RefCell::new(None));
+static DI1: Mutex<Option<LCD128x64>> = Mutex::new(RefCell::new(None));
+
+static BOOP: Mutex<bool> = Mutex::new(RefCell::new(false));
+*/
+
+static PBTN1: SpinLockMutex<Option<Gpio0<Input<PullUp>>>> = SpinLockMutex::new(None);
+static PBTN2: SpinLockMutex<Option<Gpio2<Input<PullUp>>>> = SpinLockMutex::new(None);
+static JBTN1: SpinLockMutex<Option<Gpio5<Input<PullUp>>>> = SpinLockMutex::new(None);
+static JBTN2: SpinLockMutex<Option<Gpio12<Input<PullUp>>>> = SpinLockMutex::new(None);
+static DI1: SpinLockMutex<Option<LCD128x64>> = SpinLockMutex::new(None);
+
+static BOOP: SpinLockMutex<bool> = SpinLockMutex::new(false);
+
+// unsafe impl Sync for I2C0 {}
+
+fn draw<'a, D>(display: &mut D, title: &'a str, msg: &'a str) -> Result<(), D::Error>
 where
     D: DrawTarget + Dimensions,
     D::Color: From<Rgb565>,
@@ -58,73 +95,164 @@ where
 
 #[entry]
 fn main() -> ! {
-    let mut peripherals = Peripherals::take().unwrap();
+    let mut peripherals = Peripherals::take();
 
-    let mut rtccntl = RtcCntl::new(peripherals.RTC_CNTL);
-    let mut timer0 = Timer::new(peripherals.TIMG0);
-    let mut timer1 = Timer::new(peripherals.TIMG1);
-    let serial0 = Serial::new(peripherals.UART0).unwrap();
-    // There is NO NEED TO PANIC! EVERYTHING IS OK!
-    let mut serial = PanicHandler::new(serial0);
+    let system = peripherals.SYSTEM.split();
+    let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
 
-    // Disable watchdog timer
-    rtccntl.set_wdt_global_enable(false);
-
-    timer0.disable();
-    timer1.disable();
-
-    timer0.start(30_000_000u64);
-
-    let io = gpio::IO::new(peripherals.GPIO, peripherals.IO_MUX);
-
+    // Set GPIO4 as an output, and set its state high initially.
+    let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
     let mut led = io.pins.gpio4.into_push_pull_output();
+
+    led.set_high().unwrap();
+
+    // Initialize the Delay peripheral, and use it to toggle the LED state in a
+    // loop.
+    let mut delay = Delay::new(&clocks);
+
+    let timer_group0 = TimerGroup::new(peripherals.TIMG0, &clocks);
+    let mut timer0 = timer_group0.timer0;
+    /*
+        timer0.disable();
+        timer0.start(30_000_000u64);
+    */
+
+    println!("Go go go");
+
+    // FIXME: As of now, push button 2 and joystick button 2 trigger once
+    // initially. This is an issue in the esp-hal crate.
+    // https://github.com/esp-rs/esp-hal/issues/54#issuecomment-1115306416
+    /* push buttons */
+    let mut pbtn1 = io.pins.gpio0.into_pull_up_input();
+    pbtn1.listen(Event::FallingEdge);
+    (&PBTN1).lock(|data| (*data).replace(pbtn1));
+
+    let mut pbtn2 = io.pins.gpio2.into_pull_up_input();
+    pbtn2.listen(Event::FallingEdge);
+    (&PBTN2).lock(|data| (*data).replace(pbtn2));
+
+    /* joystick buttons */
+    let mut jbtn1 = io.pins.gpio5.into_pull_up_input();
+    jbtn1.listen(Event::FallingEdge);
+    (&JBTN1).lock(|data| (*data).replace(jbtn1));
+
+    let mut jbtn2 = io.pins.gpio12.into_pull_up_input();
+    jbtn2.listen(Event::FallingEdge);
+    (&JBTN2).lock(|data| (*data).replace(jbtn2));
+
+    interrupt::enable(Interrupt::GPIO, interrupt::Priority::Priority3).unwrap();
 
     /* I2C OLED displays */
     let sda = io.pins.gpio21;
     let scl = io.pins.gpio22;
-
-    let i2c = i2c::I2C::new(
+    let i2c = I2C::new(
         peripherals.I2C0,
         sda,
         scl,
-        400_000, // 400kHz
-        &mut (peripherals.DPORT),
-    )
-    .unwrap();
+        400u32.kHz(), // 400kHz
+        &clocks,
+    );
+
+    println!("Initialize displays...");
     // Instantiate
     let i2c_bus = shared_bus::BusManagerSimple::new(i2c);
-    let di1 = ssd1306::I2CDisplayInterface::new(i2c_bus.acquire_i2c());
-    let di2 = ssd1306::I2CDisplayInterface::new_alternate_address(i2c_bus.acquire_i2c());
+    let di1 = I2CDisplayInterface::new(i2c_bus.acquire_i2c());
+    let di2 = I2CDisplayInterface::new_alternate_address(i2c_bus.acquire_i2c());
+
     // Initialize
-    let mut d1 = ssd1306::Ssd1306::new(
+    let mut d1 = Ssd1306::new(
         di1,
         ssd1306::size::DisplaySize128x64,
         ssd1306::rotation::DisplayRotation::Rotate0,
     )
     .into_buffered_graphics_mode();
-    writeln!(serial, "{:#?}", d1.init()).unwrap();
+    d1.init().expect("display 1 init");
+    // (&DI1).lock(|data| (*data).replace(d1));
 
-    let mut d2 = ssd1306::Ssd1306::new(
+    let mut d2 = Ssd1306::new(
         di2,
         ssd1306::size::DisplaySize128x64,
         ssd1306::rotation::DisplayRotation::Rotate0,
     )
     .into_buffered_graphics_mode();
-    writeln!(serial, "{:#?}", d2.init()).unwrap();
+    d2.init().expect("display 2 init");
+
+    println!("Test draw on displays...");
+
     // Draw! :)
-    draw(&mut d1, &mut serial, ">> Das Labor <<", "Write Rust!").unwrap();
+    draw(&mut d1, ">> Das Labor <<", "Write Rust!").expect("draw");
     d1.flush().unwrap();
 
-    draw(&mut d2, &mut serial, "\\o/ *woop woop* \\o/", "Party hard!").unwrap();
+    draw(&mut d2, "\\o/ *woop woop* \\o/", "Party hard!").unwrap();
     d2.flush().unwrap();
+
+    // Good to go, let the LED shine!
+    led.set_high().unwrap();
+    let mut x = 0;
+
+    println!("Initialized. Enter loop...");
 
     /* main loop :) */
     loop {
-        writeln!(serial, "====== ON =====\r").unwrap();
-        led.set_high().unwrap();
-        block!(timer0.wait()).unwrap();
-        writeln!(serial, "===== OFF =====\r").unwrap();
-        led.set_low().unwrap();
-        block!(timer0.wait()).unwrap();
+        led.toggle().unwrap();
+        delay.delay_ms(500u32);
+        if (&BOOP).lock(|data| data.clone()) {
+            (&BOOP).lock(|data| {
+                println!("boop boop");
+                x += 1;
+                let y = arrform!(13, "YEEHAW {}", x);
+                draw(&mut d2, "BOOP BOOP", y.as_str()).unwrap();
+                d2.flush().unwrap();
+                *data = false;
+            });
+        }
     }
+}
+
+#[ram]
+#[interrupt]
+unsafe fn GPIO() {
+    let lvl = xtensa_lx::interrupt::get_level();
+    println!("GPIO Interrupt with priority {lvl}",);
+
+    // TODO: Is this necessary?
+    interrupt::clear(
+        Cpu::ProCpu,
+        interrupt::CpuInterrupt::Interrupt22EdgePriority3,
+    );
+
+    /* push buttons */
+    (&PBTN1).lock(|data| {
+        let button = data.as_mut().unwrap();
+        // if button.is_interrupt_set() {
+        println!("PBTN1");
+        button.clear_interrupt();
+        // }
+    });
+    (&PBTN2).lock(|data| {
+        let button = data.as_mut().unwrap();
+        // if button.is_interrupt_set() {
+        // println!("PBTN2");
+        (&BOOP).lock(|data| {
+            *data = true;
+        });
+        button.clear_interrupt();
+        //}
+    });
+
+    /* joystick buttons */
+    (&JBTN1).lock(|data| {
+        let button = data.as_mut().unwrap();
+        // if button.is_interrupt_set() {
+        // println!("JBTN1");
+        button.clear_interrupt();
+        // }
+    });
+    (&JBTN2).lock(|data| {
+        let button = data.as_mut().unwrap();
+        // if button.is_interrupt_set() {
+        // println!("JBTN2");
+        button.clear_interrupt();
+        // }
+    });
 }
